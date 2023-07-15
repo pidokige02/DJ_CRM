77a921f3fd502d5b6ba22d23e19810132a50e221
pidokige02 <pidokige0204@gmail.com>
2023-07-15 22:07:30 +0900

    Templates and rendering them in the response

--- leads/views.py
+++ leads/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("Hello world")
+    # return render(request, "second_page.html")
+    return render(request, "leads/home_page.html")
