69bb350d765120d67f15fb467b51428764d5ccb8
pidokige02 <pidokige0204@gmail.com>
2023-07-26 21:21:57 +0900

    Password reset

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import include, path
 from leads.views import landing_page, LandingPageView, SignupView
 
@@ -28,6 +35,10 @@ urlpatterns = [
     path('polls/',  include('polls.urls', namespace="polls")),
     path('agents/', include('agents.urls', namespace="agents")),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
