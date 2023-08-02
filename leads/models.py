0e1322d5a1954d4fee32a07e31d57f1ae941b480
pidokige02 <pidokige0204@gmail.com>
2023-08-03 05:52:24 +0900

    Category model+Category list view

--- leads/models.py
+++ leads/models.py
@@ -19,6 +19,7 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -30,6 +31,12 @@ class Agent(models.Model):
     def __str__(self):
         return self.user.email
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)  # New (make it as default), Contacted, Converted, Unconverted
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 # user를 만들면 자돋으로 UserProfile 도 만들어 준다
 def post_user_created_signal(sender, instance, created, **kwargs):
