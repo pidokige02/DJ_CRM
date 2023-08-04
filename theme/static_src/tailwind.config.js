a5e7d7ac7b51af3871f9acf461f652958042fa20
pidokige02 <pidokige0204@gmail.com>
2023-08-04 18:34:54 +0900

    Safelist for message styles

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -41,6 +41,20 @@ module.exports = {
          */
         // '../../**/*.py'
     ],
+    safelist: [
+        'bg-green-50',
+        'border-green-400',
+        'bg-blue-50',
+        'border-blue-400',
+        'bg-yellow-50',
+        'border-yellow-400',
+        'bg-red-50',
+        'border-red-400',
+        'text-green-700',
+        'text-blue-700',
+        'text-yellow-700',
+        'text-red-700',
+    ],
     theme: {
         extend: {},
     },
